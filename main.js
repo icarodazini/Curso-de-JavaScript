0fd587dc4bf44819f3e4f0dcacdca01ce5a5eccb
Icaro Dazini <icarodazini@gmail.com>
2026-06-07 22:52:00 -0300

    Initial commit: add curso JavaScript files

---
+++ main.js
@@ -0,0 +1,27 @@
+// Curso de JavaScript #02 - Variáveis & Tipos de Dados
+
+// Strings
+const name =  'luiza';
+
+console.log(`My name is ${name.toUpperCase()}`);
+
+//Number
+const number = 10;
+
+console.log(number.toString());  
+
+//Boolean
+false   
+true
+
+console.log(2 == 2);
+
+// Null and Undefined
+const x = null;
+const y = undefined;
+
+const list = [1,2,3,4,5];
+const object = { name: 'Luiza', age: 30 };
+
+console.log(list[0]);
+console.log(object.name + ' ' + object.age);
